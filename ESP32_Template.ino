2944d0a2ee420669d28665cf6cb6a0947e6123f6
moeburn <moeburn@gmail.com>
2023-12-27 19:42:42 -0500

    Add files via upload

---
+++ ESP32_Template.ino
@@ -0,0 +1,101 @@
+#include <Arduino.h>
+#include <BlynkSimpleEsp32.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+#include "time.h"
+
+const char* ssid = "mikesnet";
+const char* password = "springchicken";
+
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = -18000;  //Replace with your GMT offset (secs)
+const int daylightOffset_sec = 0;   //Replace with your daylight offset (secs)
+
+char auth[] = "DU_j5IxaBQ3Dp-joTLtsB0DM70UZaEDd";
+
+AsyncWebServer server(80);
+
+WidgetTerminal terminal(V10);
+
+#define every(interval) \
+    static uint32_t __every__##interval = millis(); \
+    if (millis() - __every__##interval >= interval && (__every__##interval = millis()))
+
+BLYNK_WRITE(V0) {
+  if (String("help") == param.asStr()) {
+    terminal.println("==List of available commands:==");
+    terminal.println("wifi");
+    terminal.println("==End of list.==");
+  }
+  if (String("wifi") == param.asStr()) {
+    terminal.print("Connected to: ");
+    terminal.println(ssid);
+    terminal.print("IP address:");
+    terminal.println(WiFi.localIP());
+    terminal.print("Signal strength: ");
+    terminal.println(WiFi.RSSI());
+    printLocalTime();
+  }
+}
+
+void printLocalTime() {
+  time_t rawtime;
+  struct tm* timeinfo;
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  terminal.println(asctime(timeinfo));
+  terminal.flush();
+}
+
+void setup(void) {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! I am ESP32.");
+  });
+
+  AsyncElegantOTA.begin(&server);    // Start ElegantOTA
+  server.begin();
+  Serial.println("HTTP server started");
+  delay(250);
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  Blynk.config(auth, IPAddress(192, 168, 50, 197), 8080);
+  Blynk.connect();
+  delay(250);
+  struct tm timeinfo;
+  getLocalTime(&timeinfo);
+  hours = timeinfo.tm_hour;
+  mins = timeinfo.tm_min;
+  secs = timeinfo.tm_sec;
+  terminal.println("***SERVER STARTED***");
+  terminal.print("Connected to ");
+  terminal.println(ssid);
+  terminal.print("IP address: ");
+  terminal.println(WiFi.localIP());
+  printLocalTime();
+  terminal.flush();
+}
+
+void loop() {
+      if (WiFi.status() == WL_CONNECTED) {Blynk.run();}
+      every(1000){
+      Blynk.virtualWrite(V1, 69420);
+      }
+
+}
